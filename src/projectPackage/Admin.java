c8bf0f149f525ed08329f26ad26d3ed214d83fa4
Miguel Machado <mantonio@student.dei.uc.pt>
2015-11-20 19:49:27 +0000

    More work

--- src/projectPackage/Admin.java
+++ src/projectPackage/Admin.java
@@ -6,20 +6,49 @@ import java.util.Scanner;
 
 //TODO Ver como a criaçao de cliente e feita.
 //TODO Decidir o que os metodos recebem e devolvem.
+//TODO Testar os inputs no createClient
 
 public class Admin extends User {
     public Admin(String name, String nif, String address, String email, String phone, String password) {
         super(name, nif, address, email, phone, password);
     }
 
-    public void createClient() {
+    public void createClient(ArrayList<Client> clients) {
+        System.out.print("Input client name: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println();
+        System.out.print("Input client NIF: ");
+        String nif = input.nextLine();
+        System.out.println();
+        System.out.print("Input client address: ");
+        String address = input.nextLine();
+        System.out.println();
+        System.out.print("Input client email: ");
+        String email = input.nextLine();
+        System.out.println();
+        System.out.print("Input client phone number:");
+        String phone = input.nextLine();
+        System.out.println();
+        System.out.print("Input client password: ");
+        String password = input.nextLine();
+        System.out.println();
+        System.out.println("Premium or Regular?");
+        String type = input.nextLine();
+        if (type.equals("Premium")){
+            Premium client = new Premium(name,nif, address, email, phone, password);
+            clients.add(client);
+        } else {
+            Regular client = new Regular(name,nif, address, email, phone, password);
+            clients.add(client);
+        }
 
     }
     public void deleteClient(ArrayList<Client> clients) {
         int i = 0;
         while(i < clients.size()) {
             i++;
-            if (clients.get(i) ==  )
+            if (clients.get(i) == )
         }
     }
     public void modifyClient(Client client) {
