5678b868d40058cf18d8d051b1b1d7b9f6735c1a
PunkCosta <punkcosta@gmail.com>
2015-11-20 19:43:01 +0000

    Trip Update

--- src/projectPackage/Trip.java
+++ src/projectPackage/Trip.java
@@ -1,12 +1,45 @@
 package projectPackage;
 
 public class Trip {
+    //Atributes
     private int code;
-    private String origin;
-    private String destiny;
-    private double price;
+    private String origin, destiny;
+    private double price, duration, rating;
     private Date date;
-    private double duration;
-    private double rating;
 
+    //Constructors
+    Trip(){}
+    Trip(int code,String origin,String destiny,double price, double duration, double rating,Date date){
+        this.code = code;
+        this.origin = origin;
+        this.destiny = destiny;
+        this.price = price;
+        this.duration = duration;
+        this.rating = rating;
+        this.date = date;
+    }
+    
+    //Getters && Setters
+    public int getCode() { return code; }
+    public void setCode(int code) { this.code = code; }
+
+    public Date getDate() { return date; }
+    public void setDate(Date date) { this.date = date; }
+
+    public String getOrigin() { return origin; }
+    public void setOrigin(String origin) { this.origin = origin; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public double getRating() { return rating; }
+    public void setRating(double rating) { this.rating = rating; }
+
+    public String getDestiny() { return destiny; }
+    public void setDestiny(String destiny) { this.destiny = destiny; }
+
+    public double getDuration() { return duration; }
+    public void setDuration(double duration) { this.duration = duration; }
+
+    //
 }
